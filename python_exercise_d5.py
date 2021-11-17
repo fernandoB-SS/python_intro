1d74321d98c167c6e97eb9abbb6fd72e6486261c
Fenando Becerra <fern>
2021-11-17 11:47:48 -0800

    Correct file

--- python_exercise_d5.py
+++ python_exercise_d5.py
@@ -5,5 +5,11 @@ weight = input("Please enter your weight in kilograms: ")
 # bmi = weight / height^2
 bmi = int(weight) / float(height) ** 2
 
-# print bmi
-print(int(bmi))
+if bmi < 18.5:
+    print(f"Under weight- BMI: {int(bmi)}")
+elif bmi < 25:
+    print(f"Normal weight- BMI: {int(bmi)}")
+elif bmi < 30:
+    print(f"Over weight- BMI: {int(bmi)}")
+else:
+    print(f"Obese- BMI: {int(bmi)}")
